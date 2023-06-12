9d7fc2ddd3ba90eed1a5c2e88424f6ab6e650335
Jude Oyedele <judeokennywise@gmail.com>
2023-06-12 21:26:15 +0100

    edit

--- chatInter.py
+++ chatInter.py
@@ -34,7 +34,7 @@ The mission statement for Atlantis is enclosed within triple backticks (`) as fo
 
 Here are a list of 3 example of grants to output that we will be applying for as they align with our goals: 
 
-“****[Grants and In-Kind Support to USA Nursing and Medical Educators for Career Development](https://usa.grantwatch.com/grant/184749/grants-and-in-kind-support-to-usa-nursing-and-medical-educators-for-career-development.html)****
+'****[Grants and In-Kind Support to USA Nursing and Medical Educators for Career Development](https://usa.grantwatch.com/grant/184749/grants-and-in-kind-support-to-usa-nursing-and-medical-educators-for-career-development.html)****
 
 Deadline *09/15/23*
 
@@ -59,9 +59,35 @@ Competition for USA, Canada, and International entrepreneurs representing any bu
 
 GrantWatch ID#: 160904
 
-**[VIEW FULL GRANT »](https://usa.grantwatch.com/grant/160904/competition-for-usa-canada-and-international-nonprofit-and-for-profit-entrepreneurs-for-ventures-that-reduce-poverty.html)**“
+**[VIEW FULL GRANT »](https://usa.grantwatch.com/grant/160904/competition-for-usa-canada-and-international-nonprofit-and-for-profit-entrepreneurs-for-ventures-that-reduce-poverty.html)**'
+ Here are 3 examples of grants that does not align with our goals:
+ '****[Grants to USA, Canada, and International Nonprofits and For-Profits for Research on Problem ...](https://usa.grantwatch.com/grant/207442/grants-to-usa-canada-and-international-nonprofits-and-for-profits-for-research-on-problem-gambling-behavior.html)****
 
-Please elaborate on Atlantis' mission and align your grant selection accordingly. Include the corresponding links for each grant that matches our mission. Make sure to provide the grants in the format they were originally presented.
+Deadline *07/14/23*
+
+Grants to USA, Canada, and International nonprofit and for-profit organizations for research on problem gambling behavior. Applicants must submit an LOI prior to submitting a full proposal. Funding is intended to support projects that address important problems or crit...
+
+GrantWatch ID#: 207442
+
+**[VIEW FULL GRANT »](https://usa.grantwatch.com/grant/207442/grants-to-usa-canada-and-international-nonprofits-and-for-profits-for-research-on-problem-gambling-behavior.html)**
+****[Grants to USA Entrepreneurs for Capital Purchases](https://usa.grantwatch.com/grant/203301/grants-to-usa-entrepreneurs-for-capital-purchases.html)****
+
+Deadline *06/02/23*
+
+Grants of $25,000 for capital purchases to USA entrepreneurs who identify as Black. Funding is intended to allow small businesses to purchase a commercial vehicle to improve the functioning of the company. Applicants must demonstrate a need for a qualifying commercial ...
+
+GrantWatch ID#: 203301
+
+**[VIEW FULL GRANT »](https://usa.grantwatch.com/grant/203301/grants-to-usa-entrepreneurs-for-capital-purchases.html)**
+****[Awards to USA Teachers to Recognize Outstanding Educational Methods for Teaching Economics to ...](https://usa.grantwatch.com/grant/143081/awards-to-usa-teachers-to-recognize-outstanding-educational-methods-for-teaching-economics-to-students.html)****
+
+Deadline *06/23/23*
+
+Awards of $1,000 to USA teachers to recognize outstanding educational methods for teaching students about economics. Recipients are K-12 teachers of any subject. The goal is to reward teachers for using innovative teaching practices to cultivate a rich understanding of...
+
+GrantWatch ID#: 143081'
+
+Please elaborate on Atlantis' mission and align your grant selection accordingly, with reference to the examples provided avove too. Include the corresponding links for each grant that matches our mission. Make sure to provide the grants in the format they were originally presented.
 
 Please ensure the results are accurate and the information is presented in a manner that is clear and easy to understand. Always check for a final time if the user wants to add anything else before finalizing the process.""")
 
