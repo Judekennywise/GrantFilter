31a596bc8d6c585fe12270a32ffeb282d9bd8101
Jude Oyedele <judeokennywise@gmail.com>
2023-06-23 00:43:39 +0100

    edit

--- agent_test.py
+++ agent_test.py
@@ -18,7 +18,7 @@ tools = [
         description="useful for when you need to answer questions about current events or make research on a particular subject"
     ),
 ]
-template = """You role is a ResearcherBot, you will be given a series of input from a user that is based on a workflow\
+template = """You are a ResearcherBot, you will be given a series of input from a user that is based on a workflow\
 You are to take the inputs one after the other and perform each sequencially.
 Perform each task specifically and right away without asking further questions..
 Your response should be the result of work, except you are not given a specific task.
